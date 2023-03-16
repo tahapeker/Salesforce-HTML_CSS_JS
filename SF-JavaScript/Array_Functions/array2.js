4e353e958b5f862efea280d2e21b4020fe6bd823
Amit Kumar <amitniitmca@gmail.com>
2023-03-16 05:38:00 +0530

    Array Constructor

---
+++ SF-JavaScript/Array_Functions/array2.js
@@ -0,0 +1,12 @@
+var array1 = [1, 2, 3, 4, 5];
+console.log(array1);
+var array2 = new Array();
+console.log(array2);
+var array3 = new Array(2);
+console.log(array3);
+var array4 = new Array(12,3,4);
+console.log(array4);
+
+
+var array5 = [10, "Amit", true];
+console.log(array5);
