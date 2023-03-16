38928a886ded8c53043e81cb1159948c021d6ffa
Amit Kumar <amitniitmca@gmail.com>
2023-03-16 06:36:09 +0530

    includes and from function

---
+++ SF-JavaScript/Array_Functions/array6.js
@@ -0,0 +1,8 @@
+var fruits = ["Apple", "Banana", "Orange", "Grapes"];
+
+console.log(fruits.includes("Banana"));
+console.log(fruits.includes("Pine Apple"));
+console.log(fruits.includes("Banana", 2));
+
+var array = Array.from("123456789");
+console.log(array);
