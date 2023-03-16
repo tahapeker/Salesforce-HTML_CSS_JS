861b0b64764bbab441cd123441f4b641f89b5085
Amit Kumar <amitniitmca@gmail.com>
2023-03-16 07:11:45 +0530

    Assignment

--- SF-JavaScript/Array_Functions/assignment.js
+++ SF-JavaScript/Array_Functions/assignment.js
@@ -4,11 +4,13 @@
 var friends = ["Amit", "Farid", "Ayshan","Rashid", "Elbrus"];
 
 function nameFilter(name){
-    // return Array.from(name)[0] == 'A';
-    return name.charAt(0) == "A";
+    return Array.from(name)[0] == 'A';
+    // return name.charAt(0) == "A";
 }
 
 var newArray = friends.filter(nameFilter);
 
 console.log(newArray);
+
+console.log(Array.from("Amit"));
 
