ed34f935cd0caafab4f702d6237f7bbfd1a953e3
Amit Kumar <amitniitmca@gmail.com>
2023-03-16 07:09:03 +0530

    Assignment

---
+++ SF-JavaScript/Array_Functions/assignment.js
@@ -0,0 +1,14 @@
+// Create a JS file where you will have an array of String (name of friends)
+// and create a new array with only those names which starts with alphabet 'A'
+
+var friends = ["Amit", "Farid", "Ayshan","Rashid", "Elbrus"];
+
+function nameFilter(name){
+    // return Array.from(name)[0] == 'A';
+    return name.charAt(0) == "A";
+}
+
+var newArray = friends.filter(nameFilter);
+
+console.log(newArray);
+
