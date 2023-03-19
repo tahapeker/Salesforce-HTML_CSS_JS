0e170cbf17ee572bc5d96a7ffa7578945e5c9bd1
tahapeker <55011410+tahapeker@users.noreply.github.com>
2023-03-18 21:46:25 -0700

    DinoGame Update

--- DinosaurGame/Dino.js
+++ DinosaurGame/Dino.js
@@ -1,27 +1,30 @@
-const dinosaur = document.getElementById('dinosaur');
-
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 function jump() {
-  let position = 0;
-  let interval = setInterval(() => {
-    if (position === 150) {
-      clearInterval(interval);
-      let downInterval = setInterval(() => {
-        if (position === 0) {
-          clearInterval(downInterval);
-        } else {
-          position -= 10;
-          dinosaur.style.bottom = position + 'px';
-        }
-      }, 20);
-    } else {
-      position += 10;
-      dinosaur.style.bottom = position + 'px';
+    if (dispatchEvent.classList != "jump") {
+        //first it checks if the dino is mid-jump. If not, it makes it jump.
+        dino.classList.add("jump");
+        setTimeout(function () {
+            dino.classList.remove("jump");
+            //removes the jump class from the dino once it has jumped so that it can jump again
+        }, 300);
     }
-  }, 20);
 }
-
-document.addEventListener('keydown', (event) => {
-  if (event.code === 'Space') {
+let checkAlive = setInterval(function () {
+    let dinoTop = parseInt(
+        window.getComputedStyle(dino).getPropertyValue("top")
+    );
+    let cactusLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+    //check for collision
+    if (cactusLeft > 0 && cactusLeft < 70 && dinoTop >= 143) {
+        dino.style.animationPlayState = "paused";
+        cactus.style.animationPlayState = "paused";
+        alert("Whoops! Game Over :(");
+        window.location.reload();
+    }
+}, 10);
+document.addEventListener("keydown", function (event) {
     jump();
-  }
 });
