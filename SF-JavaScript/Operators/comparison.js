defc1c7581d89e175dcc7895832e1e817c62841e
Amit Kumar <amitniitmca@gmail.com>
2023-03-24 07:37:44 +0530

    Comparison and logical bitwise

---
+++ SF-JavaScript/Operators/comparison.js
@@ -0,0 +1,10 @@
+var var1 = 25;
+var var2 = "25";
+var var3 = -25;
+
+console.log(var1 == var2);
+console.log(var1 === var2);
+
+console.log(var1 != var2);
+console.log(var1 !== var2);
+
---
+++ SF-JavaScript/Operators/logicalOp.js
@@ -0,0 +1,19 @@
+let var1 = 10;
+let var2 = 20;
+let var3 = 30;
+
+console.log(var3 > var2 & var2 > var1);
+console.log(var2 > var1 & var1 > var2);
+
+console.log(var3 > var2 | var2 > var1);
+console.log(var2 > var1 | var1 > var2);
+
+console.log(var3 > var2 ^ var2 > var1);
+console.log(var2 > var1 ^ var1 > var2);
+
+
+
+// 1 ==> true
+// 0 ==> false
+
+
