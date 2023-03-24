e0972e59784e764a59bb372ade9239a146afdda0
Amit Kumar <amitniitmca@gmail.com>
2023-03-24 05:33:52 +0530

    Checking type of variables

---
+++ SF-JavaScript/Variables/typeoperator.js
@@ -0,0 +1,20 @@
+var var0 = 25.5;
+var var1 = 25;
+var var2 = '25';
+var var3 = true;
+var var4;
+var var5 = null;
+var var6 = [1, 2, 3];
+var var7 = {
+    type : 'Object Type',
+    age : 25
+};
+
+console.log(typeof var0);
+console.log(typeof var1);
+console.log(typeof var2);
+console.log(typeof var3);
+console.log(typeof var4);
+console.log(typeof var5);
+console.log(typeof var6);
+console.log(typeof var7);
