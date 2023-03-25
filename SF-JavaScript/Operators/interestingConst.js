cafcf716551ab70297a2719254093031421baaad
Amit Kumar <amitniitmca@gmail.com>
2023-03-25 20:13:01 +0530

    interesting thing with const keyword

---
+++ SF-JavaScript/Operators/interestingConst.js
@@ -0,0 +1,12 @@
+const student = {
+    name : 'Amit Kumar'
+};
+
+console.log(student);
+student.age = 35;
+console.log(student);
+
+const myarray = [10, 20];
+myarray[2] = 30;
+console.log(myarray);
+
