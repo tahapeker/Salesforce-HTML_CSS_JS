0f75880038d8413043e5fe908e348088ee74e385
Amit Kumar <amitniitmca@gmail.com>
2023-03-26 19:46:08 +0530

    Anonymous functions

---
+++ SF-JavaScript/Function_Types/anonymous.js
@@ -0,0 +1,9 @@
+// function(){
+//     console.log("Hello Anonymous");
+// };
+
+var arr = [12, 15, 17, 25, 50];
+
+arr.forEach(function(first, second){
+    console.log(second+' : '+first);
+});
---
+++ SF-JavaScript/Function_Types/anonymous2.js
@@ -0,0 +1,12 @@
+setTimeout(function(){
+    document.body.style.backgroundColor = 'cyan';
+}, 3000);
+
+/*
+Uses of Anonymous Function
+1. To create a function with function expression
+2. To pass a function as parameter where you don't want to create
+   a new function
+3. To pass a function in callback function(Asynchronous JS)
+4. IIFE
+*/
